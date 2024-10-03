f748953a8dfa86b21da8b47d72f11b883575ee70
kneecapFolder <ilan7gr@gmail.com>
2024-10-03 18:05:29 +0300

    added the actual audio player

--- src/render.js
+++ src/render.js
@@ -24,26 +24,79 @@ class Song {
         elm.children[0].innerText = this.name;
         elm.children[1].innerText = this.artist;
         obj.appendChild(elm)
+        this.obj = obj;
 
-        obj.onclick = () => select(obj, this);
-
-        parent.appendChild(obj);
+        this.obj.onclick = () => pickSong(this);
+        
+        parent.appendChild(this.obj);
     }
 }
 
-
 const searchBar = document.getElementById('search');
 const songList = document.querySelector('nav');
+const root = document.querySelector(':root');
 var songs = [];
-var selected;
+let playing = false;
+let changing = false;
+
+// Audio player
+const progress = document.getElementById('progress');
+const audio = document.querySelector('audio');
+const currTime = document.getElementById('currTime');
+const endTime = document.getElementById('endTime');
 
 
-// ipcRenderer.send("save", songs); // JSON.stringify(songList.children)
 
-function select(div, obj) {
-    div.children[1].children[0].style.color = 'blue';
-    select = obj;
-    console.log(obj);
+progress.addEventListener('input', () => {
+    changing = true;
+    audio.pause();
+    fillSlider();
+});
+progress.addEventListener('change', () => {
+    audio.currentTime = progress.value;
+    changing = false;
+    if (playing)
+        audio.play();
+});
+
+setInterval(() => {
+    if (!changing) {
+        progress.value = audio.currentTime;
+        fillSlider();
+    }
+}, 100);
+
+function pickSong(selected) {
+    selected.obj.children[1].children[0].style.color = 'blue';
+    audio.src = '../music/feel good inc.mp3';
+}
+
+audio.onloadedmetadata = () => {
+    endTime.innerHTML = formatTime(audio.duration.toFixed(0));
+    progress.setAttribute('max', audio.duration.toFixed(0));
+    audio.currentTime = 0;
+    progress.value = 0;
+    fillSlider();
+    currTime.innerHTML = '0:00';
+    playBtn(document.getElementById('play'));
+}
+
+audio.onended = () => {
+    // let index = songs.indexOf()
+}
+
+let formatTime = (t) => {
+    return `${parseInt(t/60)}:${parseInt(t%60) < 10 ? '0' : ''}${parseInt(t%60)}`;
+}
+
+function playBtn(btn) {
+    let paused = audio.paused;
+    btn.innerHTML = paused?
+        `<i class="fa-solid fa-circle-pause"></i>` :
+        `<i class="fa-solid fa-circle-play"></i>`;
+    if (paused) audio.play();
+    else audio.pause();
+    playing = paused;
 }
 
 async function loadSongs() {
@@ -54,6 +107,11 @@ async function loadSongs() {
                 new Song(elm).createObj(songList); // Add songs to the song list
             });
         });
+}
+
+function fillSlider() {
+    root.style.setProperty('--progress', `${progress.value / progress.max * 100}%`);
+    currTime.innerHTML = formatTime(progress.value);
 }
 
 loadSongs();
